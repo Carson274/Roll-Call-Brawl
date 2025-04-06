8a374aeed657cad87e29c054a9352adb1999d2f7
Mithun Karthikeyan <mithunkar2004@gmail.com>
2025-04-05 19:05:24 -0700

    oops actually connected them now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage/HomePage';
+import ClassPage from './ClassPage/ClassPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/class/:classId" element={<ClassPage />} />
       </Routes>
     </Router>
   );
--- client/src/ClassPage/ClassPage.tsx
+++ client/src/ClassPage/ClassPage.tsx
@@ -1,25 +1,24 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import './ClassPage.css';
-import React from "react";
-// import { UserSearchModal } from "./components/UserSearchModal";
-// import { UserList } from "./components/UserList";
-// import { UserList } from "./components/UserCard";
-
 
 function ClassPage() {
-    return (
-      <div className="classpage">
-        <h1>$100</h1>
-        <h2>CS 372</h2>
-        <h3>Competitors</h3>
-        <ul>
-          <li>Carpettt</li>
-          <li>Mitokongdrya</li>
-          <li>Mokka</li>
-          <li>Chat</li>
-        </ul>
-        <h3>Add Competitor</h3>
-      </div>
-    );
-  }
-  
-  export default ClassPage;
+  const { classId } = useParams<{ classId: string }>();
+
+  return (
+    <div className="classpage">
+      <h1>Class: {classId?.toUpperCase()}</h1>
+      <h2>Welcome to the {classId?.toUpperCase()} class page!</h2>
+      <h3>Competitors</h3>
+      <ul>
+        <li>Carpettt</li>
+        <li>Mitokongdrya</li>
+        <li>Mokka</li>
+        <li>Chat</li>
+      </ul>
+      <h3>Add Competitor</h3>
+    </div>
+  );
+}
+
+export default ClassPage;

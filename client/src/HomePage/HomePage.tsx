bd301a94d14f52550977d6d5ab31ff18debba6ed
Mithun Karthikeyan <mithunkar2004@gmail.com>
2025-04-05 17:47:01 -0700

    basic home page setup

---
+++ client/src/HomePage/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Roll Call Brawl</h1>
+      <p>$52.31</p>
+      <h2>Classes</h2>
+      <ul>
+        <li>CS 381</li>
+        <li>WR 121</li>
+        <li>AI 534</li>
+        <li>CS 461</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HomePage;

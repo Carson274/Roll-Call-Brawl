028ef63e5393cf748b636b0da16a82e7ab5a185a
Josiah Liebert <josiahliebert@gmail.com>
2025-04-05 18:42:09 -0700

    Set up basic Class Page

---
+++ client/src/ClassPage/ClassPage.tsx
@@ -0,0 +1,25 @@
+import './ClassPage.css';
+import React from "react";
+// import { UserSearchModal } from "./components/UserSearchModal";
+// import { UserList } from "./components/UserList";
+// import { UserList } from "./components/UserCard";
+
+
+function ClassPage() {
+    return (
+      <div className="classpage">
+        <h1>$100</h1>
+        <h2>CS 372</h2>
+        <h3>Competitors</h3>
+        <ul>
+          <li>Carpettt</li>
+          <li>Mitokongdrya</li>
+          <li>Mokka</li>
+          <li>Chat</li>
+        </ul>
+        <h3>Add Competitor</h3>
+      </div>
+    );
+  }
+  
+  export default ClassPage;

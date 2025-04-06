a0a6b1ceff43cf5f01e98dcd6153e1b759d11fb3
Josiah Liebert <josiahliebert@gmail.com>
2025-04-05 22:30:07 -0700

    Add types.ts

---
+++ client/src/types.ts
@@ -0,0 +1,41 @@
+export interface User {
+  username: string;
+  phone: string;
+  balance: number;
+  classes: string[]; // Array of class IDs - firebase docID
+}
+
+export interface Class {
+  title: string;
+  location: [number, number];
+  total: number;
+  dates: ClassDate[];
+  students: Classmate[];
+  numberOfClasses: number;
+}
+
+export interface Classmate {
+  username: string;
+  remainingBalance: number;
+  lostBalance: number;
+  attendance: number;
+}
+
+export interface ClassDate {
+  date: string;
+  startTime: string;
+  endTime: string;
+}
+
+export interface ClassmateAttendance {
+  username: string;
+  date: string;
+  checkInTime: string;
+  endTime: string;
+  attendanceStatus: 'on-time' | 'late' | 'missed' | 'idle';
+}
+
+export interface Building {
+  name: string;
+  location: [number, number];
+}
